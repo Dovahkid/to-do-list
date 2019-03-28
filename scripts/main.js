9e0d8f6a51e756b18c7c7ab84a1c75ae3ffd490a
Dovahkid <35874258+Dovahkid@users.noreply.github.com>
2019-03-28 10:26:14 -0400

    Fixed remove error
    Error caused when deleting a list item fixed

--- scripts/main.js
+++ scripts/main.js
@@ -1,22 +1,29 @@
 var x = 0;
 function addItem()
-{
+{ // The strange indentation is just for organization. probably not standard but...
   x += 1;
   var todoItem = document.getElementById("newItem").value;
   document.getElementById("newItem").value = "";
-
   var element = document.getElementById("todo");
 
   var node =  document.createElement("LI");
-    node.setAttribute('id', 'li' + x);
+    node.innerHTML = todoItem;
     node.setAttribute('onclick', 'completed('+x+')');
-    node.innerHTML = todoItem +"<button onclick='removeItem("+ x +")'>X</button>";
+    node.setAttribute('id', 'li' + x);
+
+  var buttonNode = document.createElement("BUTTON");
+    buttonNode.setAttribute('id', 'btn' + x);
+    buttonNode.setAttribute('onclick', "removeItem("+ x +")");
+    buttonNode.innerHTML = 'X';
+
   element.appendChild(node);
+  element.appendChild(buttonNode);
 }
 
 function removeItem(x)
 {
   var element = document.getElementById('li' + x).remove();
+  var buttonElement = document.getElementById('btn' + x).remove();
 }
 
 function completed(x)
